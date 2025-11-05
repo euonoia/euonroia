9aff7c371b4ab2ebd26390bbbbc40e1e337d8d41
Ken Española <revas4192@gmail.com>
2025-11-05 20:17:29 +0800

    initial commit

---
+++ backend/config/env.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  FRONTEND_URL: process.env.VITE_FRONTEND_URL || "http://localhost:5173",
+  FIREBASE_SERVICE_ACCOUNT_PATH:
+    process.env.FIREBASE_SERVICE_ACCOUNT_PATH || "./serviceAccountKey.json",
+  FIREBASE_SERVICE_ACCOUNT_JSON: process.env.FIREBASE_SERVICE_ACCOUNT_JSON || null,
+};
