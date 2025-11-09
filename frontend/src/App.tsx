1ecf2c135f82b021e8e0f2c53515ea5e4563ee86
Ken Española <revas4192@gmail.com>
2025-11-09 20:38:45 +0800

    initialc coomt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,19 @@ import AboutPage from "./pages/aboutpage";
 import UserList from "./pages/userlist";
 import LandingPage from "./pages/LandingPage";
 import OAuthCallback from "./components/OAuthCallBack";
-import { UserProvider, useUser } from "./context/UserContext";
-import { ThemeProvider } from "./context/ThemeContext";
 
-// Import your LessonPage here (Updated path)
+// Lessons
 import Greetings from "./pages/lessons/Greetings";
-import HTMLdocument from "./pages/lessons/learn-html-basics/HTMLdocument"; // Adjusted path
+import HTMLdocument from "./pages/lessons/learn-html-basics/HTMLdocument";
 import HTMLelements from "./pages/lessons/learn-html-basics/HTMLelements";
 import HTMLexam from "./pages/lessons/learn-html-basics/HTMLexam";
-// Optional wrapper to wait for user context to load
+
+import { useUser } from "./context/UserContext";
+
 function AppContent() {
   const { loading } = useUser();
 
-  if (loading) {
-    return <p style={{ textAlign: "center", marginTop: "2rem" }}>Loading application...</p>;
-  }
+  if (loading) return <p style={{ textAlign: "center", marginTop: "2rem" }}>Loading...</p>;
 
   return (
     <Routes>
@@ -29,12 +27,10 @@ function AppContent() {
       <Route path="/users" element={<UserList />} />
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/oauth-callback" element={<OAuthCallback />} />
-      {/* Add the new route for the HTML lesson */}
       <Route path="/lessons/greetings" element={<Greetings />} />
       <Route path="/lessons/html-basics" element={<HTMLdocument />} />
       <Route path="/lessons/html-elements" element={<HTMLelements />} />
       <Route path="/lessons/html-exam" element={<HTMLexam />} />
-      {/* Optional 404 route */}
       <Route path="*" element={<p>Page not found</p>} />
     </Routes>
   );
@@ -42,12 +38,8 @@ function AppContent() {
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <UserProvider>
-        <BrowserRouter>
-          <AppContent />
-        </BrowserRouter>
-      </UserProvider>
-    </ThemeProvider>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
